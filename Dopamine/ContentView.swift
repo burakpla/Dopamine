6e71bb84be1a15c60866217f09007e36a65f2460
burak pala <burak.palawsagt@gmail.com>
2026-02-21 21:29:01 +0300

    Initial Commit

---
+++ Dopamine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dopamine
+//
+//  Created by PortalGrup on 21.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dopamine/DopamineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DopamineApp.swift
+//  Dopamine
+//
+//  Created by PortalGrup on 21.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct DopamineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
